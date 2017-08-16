a9680ba334196132a1d98ec9bcaf5c2c3aedc10e
HouSheng Chiang <houshengchiang@gmail.com>
2017-08-16 15:13:27 -0400

    first commit

---
+++ Base Conversion/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+    'add exception to count
+    Sub Main()
+
+        Dim NumToConv As Integer
+        Dim BaseNum As Integer
+        Dim Remainder As Integer
+        Dim count As Integer
+        Dim order(count) As Integer
+        Dim result(count) As Integer
+
+        Console.WriteLine("Enter a number to convert:")
+        NumToConv = Console.ReadLine()
+        Console.WriteLine("You Entered: " & NumToConv)
+
+        Console.WriteLine("Enter a base number:")
+        BaseNum = Console.ReadLine()
+        Console.WriteLine("You entered: " & BaseNum)
+
+        Console.WriteLine("")
+
+        Do
+            count = count + 1
+            order(count) = order(count)
+            Remainder = NumToConv Mod BaseNum
+            order(count) = Remainder
+            Console.Write(order(count))
+            NumToConv = NumToConv \ BaseNum
+
+
+            'Console.ReadKey()
+        Loop While NumToConv <> 0
+
+
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
